06d026b03f4debb03ba109c58b055263fae63ced
rapirent <rapirent@gmail.com>
2018-03-24 21:28:32 +0800

    initial commit complete the dijkstra SSSP algorithm

---
+++ 丁國騰_hw0b.py
@@ -0,0 +1,69 @@
+from queue import PriorityQueue
+import numpy as np
+import re
+
+
+INF = 2147483647
+
+def stack(cur, start, table, route, graph, output):
+    if cur == start:
+        print(table[cur], end='', file=output)
+        return
+    stack(route[cur], start, table, route, graph, output)
+    print('--' +str(graph[route[cur]][cur]), end='', file=output)
+    print('--' + table[cur], end='', file=output)
+
+
+def dijkstra(source, target, graph, table):
+    dis = {key: INF for key in table.keys()}
+    route = {key: key for key in table.keys()}
+    dis[source] = 0
+    pq = PriorityQueue()
+    pq.put([0, source])
+    while not pq.empty():
+        edge = pq.get()
+        if dis[edge[1]] < edge[0]:
+            continue
+        if edge[1] not in graph:
+            continue
+        for i in graph[edge[1]]:
+            if dis[i] > (dis[edge[1]] + graph[edge[1]][i]):
+                dis[i] = dis[edge[1]] + graph[edge[1]][i]
+                route[i] = edge[1]
+                pq.put([dis[i], i])
+
+    return (dis[target], route)
+
+if __name__ == '__main__':
+    nodes_file = open('movie_nodes.txt', 'r')
+    name_table = dict()
+    id_table = dict()
+    for line in nodes_file:
+        parse = [t(s) for t, s in zip((int, str), re.search('^(\d+)\t(.+)\n$', line).groups())]
+        name_table[parse[1]] = parse[0]
+        id_table[parse[0]] = parse[1]
+
+    edges_file = open('movie_edgesw.txt', 'r')
+    # it can also be a 2 dimentional list,
+    # but we don't know whether a input file a continue number
+    graph = dict()
+    for line in edges_file:
+        parse = [t(s) for t, s in zip((int, int, float), re.search('^(\d+)\t(\d+)\t([\d.]+)\n$', line).groups())]
+        if parse[0] not in graph:
+            graph[parse[0]] = {}
+        if parse[1] not in graph:
+            graph[parse[1]] = {}
+        graph[parse[0]][parse[1]] = graph[parse[1]][parse[0]] = parse[2]
+
+
+    source = input('please input the source name:')
+    target = input('please input the target name:')
+
+    (distance, route_list) = dijkstra(name_table[source], name_table[target], graph, id_table)
+    output = open('丁國騰_hw0b.txt', 'w')
+    print('distance = ' + str(distance), file=output)
+    stack(name_table[target], name_table[source], id_table, route_list, graph, output)
+    print('', file=output)
+    nodes_file.close()
+    edges_file.close()
+    output.close()
